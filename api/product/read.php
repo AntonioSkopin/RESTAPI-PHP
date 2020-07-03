2537c39755e42014030cef67d040caaa8414ee0a
AntonioSkopin <askopin@outlook.com>
2020-07-03 23:39:27 +0200

    Added file to read products from datatabase and output it as a JSON format

---
+++ api/product/read.php
@@ -0,0 +1,49 @@
+<?php
+    // Required headers
+    header("Access-Control-Allow-Origin *"); // Can be read by everyone
+    header("Content-Type: application/json; charset=UTF-8");
+
+    // Include database and object files
+    include_once '../config/database.php';
+    include_once '../objects/product.php';
+
+    // Instantiate database and product object
+    $database = new Database();
+    $db = $database->getConnection(); // Get the database connection
+
+    // Initialize object
+    $product = new Product($db);
+
+    // Query products
+    $stmt = $product->read();
+    $num = $stmt->rowCount();
+
+    // Check if there are more than 0 records found
+    if ($num > 0) {
+        // Products array
+        $products_arr = array();
+        $products_arr["records"] = array();
+
+        // Retrieve table contents
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            // Extract row. This will make $row['name'] to just $name
+            extract($row);
+
+            $product_item=array(
+                "id" => $id,
+                "name" => $name,
+                "description" => html_entity_decode($description),
+                "price" => $price,
+                "category_id" => $category_id,
+                "category_name" => $category_name
+            );
+
+            array_push($products_arr["records"], $product_item);
+        }
+
+        // Set response code to 200 (OK)
+        http_response_code(200);
+
+        // Show products data in json format
+        echo json_encode($products_arr);
+    }
