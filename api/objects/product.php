9b482269c88cb48499ec5fdffad34fc76bcd5d2d
AntonioSkopin <askopin@outlook.com>
2020-07-04 18:58:54 +0200

    Added method to read one product by it's id

--- api/objects/product.php
+++ api/objects/product.php
@@ -70,4 +70,40 @@
             }
             return false;
         }
+
+        // Read a single record
+        function readOne() {
+
+            // Query to read a single record
+            $query = "SELECT
+                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    categories c
+                        ON p.category_id = c.id
+            WHERE
+                p.id = ?
+            LIMIT
+                0,1";
+
+            // Prepare the query statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind the id of product to be updated
+            $stmt->bindParam(1, $this->id);
+
+            // Execute the query
+            $stmt->execute();
+
+            // Get retrieved row
+            $row = $stmt->fetch(PDO:FETCH_ASSOC);
+
+            // Set the values to object properties
+            $this->name = $row['name'];
+            $this->price = $row['price'];
+            $this->description = $row['description'];
+            $this->category_id = $row['category_id'];
+            $this->category_name = $row['category_name'];
+        }
     }
